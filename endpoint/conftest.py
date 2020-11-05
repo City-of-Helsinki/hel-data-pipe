47046a0882045a8287a6a9f717eab0aa61eb603d
Aapo Rista <aapo.gitpub@rista.net>
2020-11-05 10:39:27 +0200

    Use mock producer when in test environment

--- endpoint/conftest.py
+++ endpoint/conftest.py
@@ -1,8 +1,19 @@
 import pytest
-
 from app import app as flask_app
+
+
+class KafkaMockProducer(object):
+    """A mock Kafka client implementing just send() method.
+    """
+
+    def __init__(self, **configs):
+        print(f'Initialised Kafka mock producer')
+
+    def send(self, topic, value=None, key=None, headers=None, partition=None, timestamp_ms=None):
+        print(f'KafkaMockProduser is pretending to send to {topic} a message: {value}')
 
 
 @pytest.fixture
 def app():
+    flask_app.producer = KafkaMockProducer()
     return flask_app
