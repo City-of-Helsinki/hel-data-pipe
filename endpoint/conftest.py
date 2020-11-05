6d86e1ba5f59acbd60a6bd5a4b0cd47bda3da29a
Aapo Rista <aapo.gitpub@rista.net>
2020-11-05 11:00:47 +0200

    Move pre-commit to the right location, fix f-string

--- endpoint/conftest.py
+++ endpoint/conftest.py
@@ -1,16 +1,24 @@
 import pytest
+
 from app import app as flask_app
 
 
 class KafkaMockProducer(object):
-    """A mock Kafka client implementing just send() method.
-    """
+    """A mock Kafka client implementing just send() method."""
 
     def __init__(self, **configs):
-        print(f'Initialised Kafka mock producer')
+        print("Initialised Kafka mock producer")
 
-    def send(self, topic, value=None, key=None, headers=None, partition=None, timestamp_ms=None):
-        print(f'KafkaMockProduser is pretending to send to {topic} a message: {value}')
+    def send(
+        self,
+        topic,
+        value=None,
+        key=None,
+        headers=None,
+        partition=None,
+        timestamp_ms=None,
+    ):
+        print(f"Pretending to send to {topic} a message: {value}")
 
 
 @pytest.fixture
