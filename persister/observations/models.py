e88bc26ff91e993f6c4e12b08cf45e7b12148c19
Aapo Rista <aapo.gitpub@rista.net>
2020-11-18 13:38:45 +0200

    Add initial models, admin and data for quantities

--- persister/observations/models.py
+++ persister/observations/models.py
@@ -1,3 +1,123 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Datasourcetype(models.Model):
+    """
+    A Datasourcetype is a family of devices, which are parsed using the same parser
+    """
+
+    name = models.CharField(
+        max_length=100, blank=True, editable=True, verbose_name=_("Name")
+    )
+    description = models.TextField(
+        blank=True, editable=True, verbose_name=_("Description")
+    )
+    parser = models.CharField(
+        max_length=100, blank=True, editable=True, verbose_name=_("Parser")
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.parser)
+
+
+class Datasource(models.Model):
+    """
+    A Datasource is an object, which has unique device id
+    """
+
+    datalogger = models.ForeignKey(
+        Datasourcetype, related_name="datasource", on_delete=models.CASCADE
+    )
+    devid = models.CharField(max_length=40, unique=True, db_index=True)
+    name = models.CharField(
+        max_length=100, blank=True, editable=True, verbose_name=_("Name")
+    )
+    description = models.TextField(
+        blank=True, editable=True, verbose_name=_("Description")
+    )
+    lat = models.FloatField(
+        blank=True, null=True, verbose_name=_("Latitude (dd.ddddd)")
+    )
+    lon = models.FloatField(
+        blank=True, null=True, verbose_name=_("Longitude (dd.ddddd)")
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} {}".format(self.devid, self.name)
+
+
+class Quantity(models.Model):
+    """
+    The Quantity is some kind of physical quantity, e.g. temperature, humidity,
+    """
+
+    name = models.CharField(max_length=64, verbose_name=_("Name"))  # e.g. "temperature"
+    abbreviation = models.CharField(
+        max_length=64, verbose_name=_("Abbreviation")
+    )  # e.g. "temp"
+    symbol = models.CharField(
+        max_length=64, blank=True, verbose_name=_("Symbol")
+    )  # e.g. '°C', '%'
+    description = models.TextField(blank=True, verbose_name=_("Comment"))
+    # URLs to wikipedia, wikidata, ontologies etc. e.g.
+    # https://www.wikidata.org/wiki/Q11466
+    urls = models.TextField(blank=True, verbose_name=_("Reference URLs"))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} [{}]".format(self.name, self.symbol)
+
+    class Meta:
+        verbose_name_plural = "Quantities"
+
+
+class Channel(models.Model):
+    """
+    All Dataloggers have one or more data channels.
+    All Values are related to one Channel
+    """
+
+    datalogger = models.ForeignKey(
+        Datasource, related_name="channels", on_delete=models.CASCADE
+    )
+    quantity = models.ForeignKey(
+        Quantity,
+        blank=True,
+        null=True,
+        related_name="channels",
+        on_delete=models.SET_NULL,
+    )
+    uniquename = models.CharField(
+        max_length=64, blank=True, verbose_name=_("Unique name for unit")
+    )  # e.g. "temp_1"
+    name = models.CharField(
+        max_length=64, blank=True, verbose_name=_("Name")
+    )  # e.g. "Temperature"
+    comment = models.CharField(
+        max_length=256, blank=True, verbose_name=_("Comment")
+    )  # e.g. "water"
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return " | ".join([self.datalogger.name, self.name, self.uniquename])
+
+
+class Value(models.Model):
+    """
+    Single data element, which has a measuring timestamp, a data channel and a value.
+    """
+
+    time = models.DateTimeField(db_index=True)
+    unit = models.ForeignKey(Channel, related_name="values", on_delete=models.CASCADE)
+    value = models.FloatField()
+    valid = models.BooleanField(default=True, verbose_name=_("Valid"))
+
+    def __str__(self):
+        return "{} {:.3f}".format(self.time, self.value)
