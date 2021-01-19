2a22e69cc24a61a6008dfc164a327d9e7e7f91a9
Pekka Lampila <pekka@montel.fi>
2021-01-19 12:31:44 +0200

    Handle static for production

--- persister/persister/settings.py
+++ persister/persister/settings.py
@@ -121,4 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = os.getenv("STATIC_URL", "/static/")
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(BASE_DIR, "static"))
