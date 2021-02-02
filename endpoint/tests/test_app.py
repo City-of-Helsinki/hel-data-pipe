85ee4b99a7fa22ec8011578e3ff25c847fa360a9
Mika Kaukoranta <mika.kaukoranta@mavericks.fi>
2021-02-02 16:49:37 +0200

    Add simple endpoint tests

--- endpoint/tests/test_app.py
+++ endpoint/tests/test_app.py
@@ -1,2 +1,10 @@
+import os
+
+
 def test_app(client):
     assert client.get("/").status_code == 200
+    assert client.get("/healthz").status_code == 200
+    assert client.get("/readiness").status_code == 200
+    assert client.get("/" + os.environ["ENDPOINT_PATH"]).status_code == 200
+
+    assert client.get("/not_existing_name").status_code == 404
